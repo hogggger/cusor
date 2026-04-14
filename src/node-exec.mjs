2c47266c31c3c30ed8d720067e8285757d7603cf
hogger <deuntlt@vip.qq.com>
2026-04-14 22:58:01 +0800

    feat: 增加执行脚本

---
+++ src/node-exec.mjs
@@ -0,0 +1,35 @@
+/*
+ * @Email: deuntlt@gmail.com
+ * @Author: hogger
+ * @Github: xxxx
+ * @Date: 2026-04-12 22:45:25
+ * @Description: node js 执行模块
+ */
+import {  spawn } from 'node:child_process'
+
+const command = 'ls -la';
+const cwd = process.cwd();
+
+//解析命令和参数
+const [cmd,...args] = command.split(' ');
+const child = spawn(cmd,args,{
+    cwd,
+    stdio:'inherit',//  实时输出到控制台
+    shell:true
+})
+let errorMsg = ''
+
+child.on('error',(error)=>{
+    errorMsg = error.message;
+});
+
+child.on('close',(code)=>{
+    if(code === 0){
+        process.exit(0)
+    }else{
+        if(errorMsg){
+            console.log(error(`错误：${errorMsg}`))
+        }
+        process.exit(code || 1)
+    }
+})
