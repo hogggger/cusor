88e3c194f99912dfc1319ee89293dd23a8b20b97
hogger <deuntlt@vip.qq.com>
2026-04-29 20:38:43 +0800

    feat: 编写读取文件工具

--- src/all-tools.mjs
+++ src/all-tools.mjs
@@ -13,5 +13,24 @@ import { spawn } from 'node:child_process'
 import { z } from 'zod'
 
 // 1.读取文件
+const readFileTool = tool(
+    async ({filePath}) => {
+        try{
+            const content = await fs.readFile(filePath, 'utf-8');
+            return `文件内容：\n${content}`;
+        }catch(error){
+            return `读取文件失败: ${error.message}`
+        };
+    
+    },
+    {
+        name:'read_file',
+        description:'读取指定文件内容',
+        schema:z.object({
+            filePath:z.string().describe('要读取的文件路径')
+        })
+    }
+)
 // 2.写入文件工具
+
 // 3.执行命令工具（带实时输出）
